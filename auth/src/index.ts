0b9cd2347e208987b624439eea792bb589c6249b
nirgluzman <nir.gluzman@gmail.com>
2024-03-13 09:42:46 +0100

    create basic template for auth routes

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,11 @@
 import express from 'express';
 import { json } from 'body-parser';
 
+import { currentUserRouter } from './routes/current-user';
+import { signinRouter } from './routes/signin';
+import { signoutRouter } from './routes/signout';
+import { signupRouter } from './routes/signup';
+
 const PORT = process.env.PORT || 3000;
 
 const app = express();
@@ -8,9 +13,10 @@ const app = express();
 // middleware to parse incoming JSON requests and put the parsed data in req.body
 app.use(json());
 
-app.get('/api/users/currentuser', (req, res) => {
-  res.send('Hi there!');
-});
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
 
 // start listening to incoming requests from a client
 app.listen(PORT, () => console.log(`Auth is up! Listening on port ${PORT}`));
