d933ace13447fc64e27a82707405e7f51e2f5d55
nirgluzman <nir.gluzman@gmail.com>
2024-04-18 08:29:51 +0200

    add a comment on Error class to invoke Object.setPrototypeOf(this, ...)

--- auth/src/errors/database-connection-error.ts
+++ auth/src/errors/database-connection-error.ts
@@ -3,7 +3,7 @@ export class DatabaseConnectionError extends Error {
   constructor() {
     super();
 
-    // Only because we are extending a built-in class (Error)
+    // Only because we are extending a built-in class (Error), https://stackoverflow.com/questions/76159041/why-do-error-subclasses-invoke-object-setprototypeofthis-in-the-construct
     Object.setPrototypeOf(this, DatabaseConnectionError.prototype);
   }
 }
--- auth/src/errors/request-validation-error.ts
+++ auth/src/errors/request-validation-error.ts
@@ -4,7 +4,7 @@ export class RequestValidationError extends Error {
   constructor(public errors: ValidationError[]) {
     super();
 
-    // Only because we are extending a built-in class (Error)
+    // Only because we are extending a built-in class (Error), https://stackoverflow.com/questions/76159041/why-do-error-subclasses-invoke-object-setprototypeofthis-in-the-construct
     Object.setPrototypeOf(this, RequestValidationError.prototype);
   }
 }
