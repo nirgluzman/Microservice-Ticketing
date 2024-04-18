a88232d9543b947152cdc21dac372f8b9c569f46
nirgluzman <nir.gluzman@gmail.com>
2024-04-18 10:36:41 +0200

    define an interface to force the type of the class

--- auth/src/errors/request-validation-error.ts
+++ auth/src/errors/request-validation-error.ts
@@ -1,6 +1,12 @@
 import { ValidationError } from 'express-validator';
 
-export class RequestValidationError extends Error {
+// define an interface to force the type of the class.
+interface CustomError {
+  statusCode: number;
+  serializeErrors(): { message: string; field?: string }[];
+}
+
+export class RequestValidationError extends Error implements CustomError {
   statusCode = 400;
 
   constructor(public errors: ValidationError[]) {
@@ -12,10 +18,9 @@ export class RequestValidationError extends Error {
 
   serializeErrors() {
     // https://www.udemy.com/course/microservices-with-node-js-and-react/learn/lecture/37727290#overview
+    // https://www.udemy.com/course/microservices-with-node-js-and-react/learn/lecture/37727992#overview
     return this.errors.map(err => {
-      if (err.type === 'field') {
-        return { message: err.msg, field: err.path };
-      }
+      if (err.type === 'field') return { message: err.msg, field: err.path };
     });
   }
 }
