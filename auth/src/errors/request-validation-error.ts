84e1cf4c176f3f3bbf6c970c754093a403ce3a86
nirgluzman <nir.gluzman@gmail.com>
2024-05-07 00:30:49 +0200

    implement CustomError abstract class

--- auth/src/errors/request-validation-error.ts
+++ auth/src/errors/request-validation-error.ts
@@ -1,16 +1,17 @@
 import { ValidationError } from 'express-validator';
+import { CustomError } from './custom-error';
 
 // define an interface to force the type of the class.
-interface CustomError {
-  statusCode: number;
-  serializeErrors(): { message: string; field?: string }[];
-}
+// interface CustomError {
+//   statusCode: number;
+//   serializeErrors(): { message: string; field?: string }[];
+// }
 
-export class RequestValidationError extends Error implements CustomError {
+export class RequestValidationError extends CustomError {
   statusCode = 400;
 
   constructor(public errors: ValidationError[]) {
-    super();
+    super('Invalid request parameters');
 
     // Only because we are extending a built-in class (Error), https://stackoverflow.com/questions/76159041/why-do-error-subclasses-invoke-object-setprototypeofthis-in-the-construct
     Object.setPrototypeOf(this, RequestValidationError.prototype);
@@ -20,7 +21,10 @@ export class RequestValidationError extends Error implements CustomError {
     // https://www.udemy.com/course/microservices-with-node-js-and-react/learn/lecture/37727290#overview
     // https://www.udemy.com/course/microservices-with-node-js-and-react/learn/lecture/37727992#overview
     return this.errors.map(err => {
-      if (err.type === 'field') return { message: err.msg, field: err.path };
+      if (err.type === 'field') {
+        return { message: err.msg, field: err.path };
+      }
+      return { message: err.msg };
     });
   }
 }
--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -2,18 +2,10 @@
 // The objective - sending a very consistent error message to the client (React application).
 
 import { Request, Response, NextFunction } from 'express';
-import { RequestValidationError } from '../errors/request-validation-error';
-import { DatabaseConnectionError } from '../errors/database-connection-error';
+import { CustomError } from '../errors/custom-error';
 
 export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
-  if (err instanceof RequestValidationError) {
-    console.log('handling this error as a request validation error');
-
-    return res.status(err.statusCode).send({ errors: err.serializeErrors() });
-  }
-
-  if (err instanceof DatabaseConnectionError) {
-    console.log('handling this error as a database connection error');
+  if (err instanceof CustomError) {
     return res.status(err.statusCode).send({ errors: err.serializeErrors() });
   }
 
